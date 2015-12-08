e52bfc39f942b046843f2b54cb5a4bae5ae1f32a
sjohst <sebjohst@gmail.com>
2015-12-08 14:46:48 +0100

    first scene

---
+++ CarboardFirstProject/Assets/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public float autoLoadNextLevelAfter=3;
+	
+	public void Start(){
+		if (autoLoadNextLevelAfter != 0) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	
+	public void LoadLevel(string name){
+		Debug.Log ("New level load: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void Quit(){
+		Debug.Log ("Quit now");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
