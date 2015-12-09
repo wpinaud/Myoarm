afc96f6b71251d41ab693e2052dbfe5dd491768e
Willis Pinaud <willispinaud@gmail.com>
2015-12-09 18:39:30 +0100

    Add new project

---
+++ MyoArmProject/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+	
+	public float autoLoadNextLevelAfter=3;
+	public float fadeInStart=1;
+
+	private GameObject[] logos;
+	private Color color = new Color(255,255,255,0);
+	
+	public void Start(){
+		logos = GameObject.FindGameObjectsWithTag ("logo");
+		if (autoLoadNextLevelAfter != 0) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	
+	public void LoadLevel(string name){
+		Debug.Log ("New level load: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void Quit(){
+		Debug.Log ("Quit now");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void Update(){
+		if (Time.time > fadeInStart) {
+			color = new Color (color.r, color.g, color.b, color.a + 0.01f);
+			if (logos.Length > 1) {
+				logos [0].GetComponents<Image> ()[0].color = color;
+				logos [1].GetComponents<Image> ()[0].color = color;
+			}
+		}
+	}
+}
---
+++ MyoArmProject/Assets/expand.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class expand : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localScale.x < 1)
+			transform.localScale += new Vector3 (0.01f, 0.01f,0.01f);
+	}
+}
